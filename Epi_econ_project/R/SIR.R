cec21349b7955f188644f04ad4b4c35c1b4c7e64
lwillem <lander.willem@uantwerpen.be>
2025-02-12 14:55:59 +0100

    Resolved issue on HealthCost
    The discounting and accumulation of the health cost was not correct. The cost at time step t should be
    "exp(-(rho + delta) * t) * gamma * kappa * Ni". In addition, removed all non-SIR code and parameters.

--- Epi_econ_project/R/SIR.R
+++ Epi_econ_project/R/SIR.R
@@ -15,35 +15,37 @@ parameters <- list(
   beta = 3/10 + 1/7,       # Transmission rate (β)
   delta = 0.67 / 365,      # Arrival rate of cure (δ)
   rho = 0.05 / 365,        # Discounting rate (ρ)
-  pi = 0.0062,             # Infection fatality rate (π)
   kappa = 197,             # Expected cost of infection (κ)
   ni0 = 0.0000527,         # Initial infected population (Ni0)
-  ns0 = 0.9999223,         # Initial susceptible population (Ns0)  (Table 1, page 15)
-  nr0 = 0.00002484,        # Initial recovered population (Nr0)
-  nd0 = 0.000000156,       # Initial dead population (Nd0)
-#  fx = 123,                # Exchange rate multiplier for USD conversion
-  export = FALSE,          # Export results flag
-  utility_type = "Log"     # Utility type: "Log" or "Quadratic"
+  ns0 = 0.9999223          # Initial susceptible population (Ns0)  (Table 1, page 15)
 )
 
 # Basic reproduction number R0 = beta / gamma
 # Table 1, page 15: R0 = 3.1
 parameters$beta / parameters$gamma
 
+# boundaries
+nifinal = 10^-9;
+
 # Define SIR model with cost tracking
-sir_costate_model <- function(time, state, parameters) { 
+sir_model <- function(time, state, parameters) { 
   with(as.list(c(state, parameters)), {
     
     # Extract state variables
     Ns <- state["Ns"]        
     Ni <- state["Ni"]        
     
+    # stopping condition
+    if(Ni < nifinal){
+      return(list(rep(0,3)))
+    }
+    
     # SIR dynamics
     dNs <- -beta * Ns * Ni
     dNi <- beta * Ns * Ni - gamma * Ni
     
-    # Corrected HealthCost equation
-    dHealthCost <- (rho + delta) * HealthCost - gamma * kappa * Ni
+    # HealthCost equation
+    dHealthCost <- exp(-(rho + delta) * time) * gamma * kappa * Ni
 
     # Return rate of change for each state variable
     return(list(c(dNs, dNi, dHealthCost)))
@@ -60,11 +62,9 @@ time_sequence <- seq(0, 200, by = 1)
 output <- ode(
   y = initial_state, 
   times = time_sequence, 
-  func = sir_costate_model, 
+  func = sir_model, 
   parms = parameters,
-  method = "lsoda"#,  # Adaptive ODE solver
-  # rootfun = root_function,
-  # events = list(func = event_function, root = TRUE)
+  method = "lsoda"
 )
 
 # Convert output to data frame
