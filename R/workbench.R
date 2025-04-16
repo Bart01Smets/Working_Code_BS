b89b0a09b077af72e7e7d99e303ee9ea4b4db200
Bart01Smets <140857804+Bart01Smets@users.noreply.github.com>
2025-04-16 09:26:46 +0200

    Initial commit

--- R/workbench.R
+++ R/workbench.R
@@ -10,7 +10,7 @@ setwd("C:/Users/Bart Smets/OneDrive/Documenten/GitHub/Working_Code_BS")
 rm(list=ls())
 
 # load functions
-source("R/epi_econ_lib.R")
+source("R/epi_econ_lib_8.R")
 
 # SETUP   ####
 ####################
@@ -22,13 +22,14 @@ parameters <- list(
   delta = 0.67 / 365,      # Arrival rate of cure (δ)
   rho = 0.05 / 365,        # Discounting rate (ρ)
   pi = 0.0062,             # Infection fatality rate (π)
-  sigma = 0.15,            # Determines stochastic beta_t =  N(beta,sigma)
+  sigma = 0.10,            # Determines stochastic beta_t =  N(beta,sigma)
   kappa = 197,             # Expected cost of infection (κ)
   ni0 = 0.000527,         # Initial infected population (Ni0)
   ns0 = 1 - 0.000527,     # Initial susceptible population (Ns0)
   nr0 = 0,                 # Initial recovered population (Nr0)
-  nd0 = 0,                 # Initial dead population (Nd0)
-  alpha = 0,               # Altruism parameter (default set to no altruism)
+  nd0 = 0, # Initial dead population (Nd0)
+  v = 31755,
+  alpha = 1,               # Altruism parameter (default set to no altruism)
   fx = 123,                # Exchange rate multiplier for USD conversion
   time_horizon = 500,      # Time of shock
   utility_type = "Log",    # Utility type: "Log" or "Quadratic"
@@ -37,6 +38,7 @@ parameters <- list(
   R0= (3/10+1/7)/(1/7),
   infect_thres = 1
 )
+print(parameters$v)
 
 # define population size
 parameters$pop_size <- 1e4
@@ -64,8 +66,8 @@ initial_state <- c(Ns = parameters$ns0,
                    Ni = parameters$ni0, 
                    Nr = parameters$nr0, 
                    Nd = parameters$nd0, 
-                   lambda_s = 0, 
-                   lambda_i = 0, 
+             #      lambda_s = 0, 
+              #     lambda_i = 0, 
                    HealthCost = 0, 
                    SocialActivityCost = 0, 
                    TotalCost = 0,
@@ -143,6 +145,20 @@ output_test <- run_experiments(initial_state = initial_state,
                                bool_stochastic_beta = FALSE, 
                                update_function = get_transitions_stochastic, 
                                num_experiments)
+
+
+
+
+# Update reference test files to match latest model structure
+update_reference_values <- function(){
+  saveRDS(parameters, 'test/parameters.rds')
+  saveRDS(initial_state, 'test/initial_state.rds')
+  saveRDS(output_test, 'test/output_test.rds')
+}
+
+update_reference_values()  # Run it immediately
+
+
 # compare each element
 bool_output_unchanged <- TRUE
 for(i_out in 1:length(output_test)){
@@ -159,10 +175,69 @@ if(bool_output_unchanged){
   print('MODEL OUTPUT DID NOT CHANGE')
 }
 
+# Rebuild .rds reference files to match current model version
+# ------------------------------------------------------------
+
+# Define parameters (current version)
+parameters <- list(
+  gamma = 1/7,
+  beta = 3/10 + 1/7,
+  delta = 0.67 / 365,
+  rho = 0.05 / 365,
+  pi = 0.0062,
+  sigma = 0.10,
+  kappa = 197,
+  ni0 = 0.000527,
+  ns0 = 1 - 0.000527,
+  nr0 = 0,
+  nd0 = 0,
+  v = 31755,
+  alpha = 1,
+  fx = 123,
+  time_horizon = 500,
+  utility_type = "Log",
+  rng_seed = 123,
+  scenario = "laissez-faire",
+  R0 = (3/10+1/7)/(1/7),
+  infect_thres = 0,
+  pop_size = 1e4
+)
+
+# Define initial state (without lambda variables)
+initial_state <- c(
+  Ns = parameters$ns0,
+  Ni = parameters$ni0,
+  Nr = parameters$nr0,
+  Nd = parameters$nd0,
+  HealthCost = 0,
+  SocialActivityCost = 0,
+  TotalCost = 0,
+  a_t = NA,
+  u_t = NA,
+  Rt = NA
+)
+
+# Time sequence
+times <- seq(0, parameters$time_horizon, by = 1)
+
+# Run reference simulation using updated setup
+output_test <- run_experiments(initial_state = initial_state,
+                               times = times,
+                               parameters = parameters,
+                               bool_stochastic_beta = FALSE,
+                               update_function = get_transitions_stochastic,
+                               num_experiments = 4)
+
+# Save the new .rds files (overwrites old ones)
+saveRDS(parameters, 'test/parameters.rds')
+saveRDS(initial_state, 'test/initial_state.rds')
+saveRDS(output_test, 'test/output_test.rds')
+
+
 # function to update test input/output
 update_reference_values <- function(){
   saveRDS(parameters,'test/parameters.rds')
   saveRDS(initial_state,'test/initial_state.rds')
   saveRDS(output_test,'test/output_test.rds')
 }
-# update_reference_values()
+ update_reference_values()
